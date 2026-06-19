6a35be8f60292bbb34f512bb7d39cee06efa28d8
Nedodoud <nedodoud@gmail.com>
2026-06-19 13:54:47 +0300

    screenshots

--- src/App.vue
+++ src/App.vue
@@ -101,12 +101,13 @@ function resetAllData() {
   });
 }
 
+/*
 function formatTime(seconds: number): string {
   const mins = Math.floor(seconds / 60);
   const secs = seconds % 60;
 
   return `${String(mins).padStart(2, "0")}:${String(secs).padStart(2, "0")}`;
-}
+}*/
 
 onMounted(() => {
   showTutorialWelcome.value = true;
